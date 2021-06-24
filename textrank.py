02faef2703c3ac00c2af4d470d3a76981c84bd73
Yeongbi <61492320+Yeongbi-Na@users.noreply.github.com>
2021-06-24 22:11:57 +0900

    Add files via upload

---
+++ textrank.py
@@ -0,0 +1,112 @@
+from collections import Counter
+from konlpy.tag import Kkma
+import PreProc as pp
+from collections import defaultdict
+
+def tokenize(text): #str형
+    noun=[] 
+    temp_result=Kkma().pos(text)
+    #명사만 추출하기
+    for i in range(len(temp_result)):
+        if temp_result[i][1]=='NNG' or temp_result[i][1]== 'NNP' :
+            noun.append(temp_result[i][0])
+    return noun #list 형 반환
+
+
+def ExtractBrand(brand_dic_file,noun): #brand_dic.txt/ datda: 전처리된 데이터
+    #브랜드사전 불러오기
+    file_name = brand_dic_file
+    with open(file_name,'rt',encoding='utf8') as f:
+        brand_dic=f.read()    
+    brand_dic=brand_dic.split("\n")
+    
+    #명사 리스트 추출 교집합 찾기
+    intersection=[]
+    intersection.append(list(set(brand_dic) & set(noun)))
+
+    return intersection
+
+
+def scan_vocabulary(sents, tokenize, min_count=2):
+    #명사 추출
+    temp= [w for sent in sents for w in tokenize(sent)]
+    #불용어 제거
+    temp=pp.remove_stopwords(temp)
+    #브랜드명 추출
+    brand_keywords=ExtractBrand('brand_dic.txt', temp)
+    brand_keywords=brand_keywords[0]
+    #명사 리스트에서 브랜드명 제거
+    noun=[]
+    for w in temp:
+        if w not in brand_keywords:
+            noun.append(w)
+     
+    #textrank 적용
+    counter = Counter(noun)
+    counter = {w:c for w,c in counter.items() if c >= min_count}
+    idx_to_vocab = [w for w, _ in sorted(counter.items(), key=lambda x:-x[1])]
+    vocab_to_idx = {vocab:idx for idx, vocab in enumerate(idx_to_vocab)}
+    return idx_to_vocab, vocab_to_idx, brand_keywords
+    
+
+def cooccurrence(tokens, vocab_to_idx, window=2, min_cooccurrence=2):
+    counter = defaultdict(int)
+    for s, tokens_i in enumerate(tokens):
+        vocabs = [vocab_to_idx[w] for w in tokens_i if w in vocab_to_idx]
+        n = len(vocabs)
+        for i, v in enumerate(vocabs):
+            if window <= 0:
+                b, e = 0, n
+            else:
+                b = max(0, i - window)
+                e = min(i + window, n)
+            for j in range(b, e):
+                if i == j:
+                    continue
+                counter[(v, vocabs[j])] += 1
+                counter[(vocabs[j], v)] += 1
+    counter = {k:v for k,v in counter.items() if v >= min_cooccurrence}
+    n_vocabs = len(vocab_to_idx)
+    return dict_to_mat(counter, n_vocabs, n_vocabs)
+    
+from scipy.sparse import csr_matrix
+
+def dict_to_mat(d, n_rows, n_cols):
+    rows, cols, data = [], [], []
+    for (i, j), v in d.items():
+        rows.append(i)
+        cols.append(j)
+        data.append(v)
+    return csr_matrix((data, (rows, cols)), shape=(n_rows, n_cols))
+    
+def word_graph(sents, tokenize=None, min_count=2, window=2, min_cooccurrence=2): #sent는 list of str
+    idx_to_vocab, vocab_to_idx, brand_keywords = scan_vocabulary(sents, tokenize, min_count)
+    tokens = [tokenize(sent) for sent in sents]
+    g = cooccurrence(tokens, vocab_to_idx, window, min_cooccurrence)#, verbose=False
+    return g, idx_to_vocab, brand_keywords
+
+
+import numpy as np
+from sklearn.preprocessing import normalize
+
+def pagerank(x, df=0.85, max_iter=30):
+    assert 0 < df < 1
+
+    # initialize
+    A = normalize(x, axis=0, norm='l1')
+    R = np.ones(A.shape[0]).reshape(-1,1)
+    bias = (1 - df) * np.ones(A.shape[0]).reshape(-1,1)
+
+    # iteration
+    for _ in range(max_iter):
+        R = df * (A * R) + bias
+
+    return R    
+    
+def textrank_keyword(sents, tokenize, min_count, window, min_cooccurrence, df=0.85, max_iter=30, topk=30):
+    g, idx_to_vocab,brand_keywords = word_graph(sents, tokenize, min_count, window, min_cooccurrence)
+    R = pagerank(g, df, max_iter).reshape(-1)
+    idxs = R.argsort()[-topk:]
+    keywords = [(idx_to_vocab[idx], R[idx]) for idx in reversed(idxs)]
+    return keywords,brand_keywords    
+    
